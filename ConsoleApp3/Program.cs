ad6186ec9ed6eae985af1c1f499d45205f6f5b3d
Lukas <lukas.karlsson@chasacademy.se>
2025-10-04 21:22:55 +0200

    First commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int size = ReadSize();
+            RenderBoard(size);
+        
+        }
+
+        static int Readsize()
+        {
+            int n;
+            while (true)
+            {
+                 Console.WriteLine("Hur stort schackbräde vill du ha? (3-50)");
+}                string input = Console.ReadLine();
+                if (int.TryParse(input, out n) && n >= 3 && n <= 50)
+                {
+                    return n;
+                }
+                Console.WriteLine("Ogiltig inmatning. Ange ett heltal mellan 3 och 50.");
+        }
+        }
+    }  
